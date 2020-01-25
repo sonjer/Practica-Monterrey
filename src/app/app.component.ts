d8256d4d6c5a0864f3239fb78fe9dfc5484d8851
Jerson Morgado <jerson.morgado@nkg.coffee>
2020-01-25 16:16:50 -0600

    subida de proyecto inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "gr-login",
+    moduleId: module.id,
+    templateUrl: "./login/login.component.html"
+})
+export class AppComponent {}
